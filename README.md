928d6172b60c0d59fd47af6775b71c34d5fc2f9a
arunthampi <arun.thampi@gmail.com>
2018-04-25 20:52:58 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Example IPFS App
+
+`main.go` is borrowed from
+[http://ipfs.git.sexy/sketches/minimal_ipfs_node.html](http://ipfs.git.sexy/sketches/minimal_ipfs_node.html)
+
+## Vendor'ed IPFS
+
+`go-ipfs` has been vendor'ed using `[govendor](https://github.com/kardianos/govendor)` and following these [instructions](https://medium.com/@iamclovin/use-ipfs-as-a-library-in-your-golang-app-3c8c4b12fe6d)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	commands "github.com/ipfs/go-ipfs/commands"
+	core "github.com/ipfs/go-ipfs/core"
+	corehttp "github.com/ipfs/go-ipfs/core/corehttp"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	nd, err := core.NewNode(ctx, &core.BuildCfg{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cctx := commands.Context{
+		Online: true,
+		ConstructNode: func() (*core.IpfsNode, error) {
+			return nd, nil
+		},
+	}
+
+	list, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("listening on: ", list.Addr())
+
+	if err := corehttp.Serve(nd, list, corehttp.CommandsOption(cctx)); err != nil {
+		log.Fatal(err)
+	}
+}
